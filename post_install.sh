28c61dec8e654f46143d75cad69e7e4a528ec046
kaef <kaef@localhost.localdomain>
2015-06-28 18:22:57 +0200

    Added codecs

--- post_install.sh
+++ post_install.sh
@@ -3,6 +3,9 @@
 #install 
 sudo dnf install -y  emacs tmux zsh p7zip mplayer
 
+##optional 
+# codecs
+#sudo dnf install -y gstreamer gstreamer-ffmpeg gstreamer-plugins-bad gstreamer-plugins-bad-free gstreamer-plugins-bad-free-extras gstreamer-plugins-bad-nonfree gstreamer-plugins-base gstreamer-plugins-good gstreamer-plugins-ugly faad2 faac libdca wget compat-libstdc++-33 compat-libstdc++-296  xine-lib-extras-freeworld
 
 #create directory tree
 mkdir ~/Workspace
