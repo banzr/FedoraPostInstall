65c26550ff15d5fcf650f23d5ef7bb68f685a11b
Andrew <mav2k5@gmail.com>
2015-10-31 15:45:32 -0400

    Create InstallApps.sh

---
+++ InstallApps.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+### Installs collection of Apps
+sudo dnf update
+
+### Repositories
+
+# RPMFusion
+su -c 'yum install --nogpgcheck \
+http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E\
+ %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusi\
+on-nonfree-release-$(rpm -E %fedora).noarch.rpm'/
+
+# Google
+su -c 'cat << EOF > /etc/yum.repos.d/google-chrome.repo
+[google-chrome]
+name=google-chrome - \$basearch
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
+EOF'
+
+# Numix
+dnf copr -y enable satya164/numix
+
+sudo dnf update
+
+### Group Install
+sudo dnf groupinstall 'Development Tools' 'Fedora Eclipse' \
+'Engineering and Scientific'/
+
+### Apps
+sudo dnf install \
+google-chrome-stable \
+unrar \
+p7zip \
+pithos \
+gnome-tweak-tool \
+numix-icon-theme-circle \
+https://github.com/atom/atom/releases/download/v1.1.0/atom.x86_64.rpm \
+vlc \
+steam \
+nautilus-open-terminal \
+deluge \
+snes9x \
+yumex-dnf \
