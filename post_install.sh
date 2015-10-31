5531ab7c4237c46dc0417885e37ead47b47302fa
Andrew <mav2k5@gmail.com>
2015-10-31 15:46:51 -0400

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,14 +1,10 @@
-#/bin/bash
+#!/bin/bash
 
-#install 
-sudo dnf install -y  emacs tmux zsh p7zip mplayer
+### Calls on dependencies
 
-##optional 
-# codecs
-#sudo dnf install -y gstreamer gstreamer-ffmpeg gstreamer-plugins-bad gstreamer-plugins-bad-free gstreamer-plugins-bad-free-extras gstreamer-plugins-bad-nonfree gstreamer-plugins-base gstreamer-plugins-good gstreamer-plugins-ugly faad2 faac libdca wget compat-libstdc++-33 compat-libstdc++-296  xine-lib-extras-freeworld
-
-#create directory tree
-mkdir ~/Workspace
-
-#Lost changes
+sudo -c
+'nohup bash InstallApps.sh &
+nohup bash InstallRacket.sh &
+nohup bash InstallIntelliJ.sh &
+nohup bash ChangeWallpaper.sh &'
 
