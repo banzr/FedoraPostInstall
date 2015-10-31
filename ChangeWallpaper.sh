b5d0206f9746e064da2a8caaf5c1b308baa1d9f5
Andrew <mav2k5@gmail.com>
2015-10-31 15:45:06 -0400

    Create ChangeWallpaper.sh

---
+++ ChangeWallpaper.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+### Desktop wallpaper
+wget -P $HOME/Pictures http://www.deviantart.com/download/485775788/mountain_by\
+_mahm0udwally-d817uzw.rar?token=b3ac016696636d1ae9b348649e77b537907c997f&ts=144\
+6259009 /
