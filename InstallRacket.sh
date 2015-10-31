6509a5e96c02aef9e1f8ad4fbd5201254cd79032
Andrew <mav2k5@gmail.com>
2015-10-31 15:47:33 -0400

    Create InstallRacket.sh

---
+++ InstallRacket.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Install Racket
+wget -P $HOME/Downloads http://mirror.racket-lang.org/installers/6.2.1/racket-\
+6.2.1-src.tgz /
+tar xzvf $HOME/Downloads/racket-6.2.1-src.tgz
+cd $HOME/Downloads/racket-6.2.1/src
+mkdir build
+cd build
+../configure
+make
+make install
