a4ca36b7992cc671b7b01cdada22fd19c34006bd
Andrew <mav2k5@gmail.com>
2015-11-01 22:06:56 -0500

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -2,9 +2,8 @@
 
 ### Calls on dependencies
 
-sudo -c
-'nohup bash InstallApps.sh &
+nohup bash InstallApps.sh &
 nohup bash InstallRacket.sh &
 nohup bash InstallIntelliJ.sh &
-nohup bash ChangeWallpaper.sh &'
+nohup bash ChangeWallpaper.sh &
 
