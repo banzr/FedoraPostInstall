b8841e6cf3315a5275a062648a25c06331dd0b08
Andrew <mav2k5@gmail.com>
2015-11-09 09:34:51 -0500

    Update InstallintelliJ.sh

--- InstallintelliJ.sh
+++ InstallintelliJ.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ### IntelliJ Installation (Must come last, stalls script)
-wget -P $HOME/Downloads https://download.jetbrains.com/idea/ideaIU-14.1.5.tar.gz
-tar xzvf $HOME/Downloads/ideaIU-14.1.5.tar.gz
-./$HOME/Downloads/ideaIU-14.1.5/bin/idea.sh
+wget -P $HOME/Downloads https://download.jetbrains.com/idea/ideaIU-15.0.tar.gz
+tar xzvf $HOME/Downloads/ideaIU-15.0.tar.gz
+./$HOME/Downloads/ideaIU-15.0/bin/idea.sh
